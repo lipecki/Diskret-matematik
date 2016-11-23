9d0cd7dc89a90e4914c8ca98e63daa6e60a01a02
ViggoL <viggo.lunden@gmail.com>
2016-11-23 20:31:48 +0100

    Revert "Påbörjat en split iaf"
    This reverts commit a315c6e21ad6c6e7b76ca8e3721f3b5f373e8c31.

--- src/CipherSender.java
+++ src/CipherSender.java
@@ -10,89 +10,26 @@ class CipherSender implements Runnable {
     private BigInteger e, N, encryptedSecret;
     private String secret;
     boolean cont = true;
-    private BigInteger keyLength;
 
-    public CipherSender(PrintWriter out, String secret, BigInteger e, BigInteger N, String keyLength) {
+    public CipherSender(PrintWriter out,String secret, BigInteger e, BigInteger N) {
         this.scan = new Scanner(System.in);
         this.out = out;
         this.secret = secret;
         this.e = e;
         this.N = N;
-        this.keyLength = new BigInteger(keyLength);
     }
 
-    private BigInteger listSum(List<BigInteger> list)
-    {
-        BigInteger val = BigInteger.ZERO;
-        for (int i = 0; i < list.size(); i++)
-        {
-            val = val.add(list.get(i));
-        }
-        return val;
-    }
-    
-    private BigInteger setAllBits(BigInteger keyLength)
-    {
-        BigInteger val = BigInteger.ZERO;
-        for (int i = 0; keyLength.compareTo(new BigInteger(String.valueOf(i))) > 0; i++)
-        {
-            val = val.setBit(i);
-        }
-        return val;
-    }
-    
-    private List<BigInteger> splitToSmaller(String secret)
-    {
-        BigInteger biSecret = new BigInteger(secret);
-        BigInteger secretBitLength = new BigInteger(String.valueOf(biSecret.bitLength()));
-        
-        List<BigInteger> secrets = new ArrayList<>();
-        
-        while (biSecret.bitLength() > listSum(secrets).bitLength())
-        {
-           
-            secrets.add(setAllBits(keyLength.subtract(BigInteger.ONE)));
-            int sumListLength = listSum(secrets).bitLength();
-            int bb = 0;
-            
-        }
-        secrets.add(biSecret.subtract(listSum(secrets)));
-        System.out.println("Split To Smaller Sum: " + listSum(secrets).toString());
-        return secrets;
-    }
-    
     public void run() {
-        
-        List<BigInteger> secrets = splitToSmaller(secret);
-        System.out.println("Split secrets:");
-        for (int i = 0; i < secrets.size(); i++) System.out.println(secrets.get(i).toString());
-        
-        
-        List<BigInteger> encryptedSecrets = new ArrayList<>();
-        for (int i = 0; i < secrets.size(); i++)
-        {
-            encryptedSecrets.add(Cryptographer.encrypt(secrets.get(i).toString(), e, N));
-        }
-        
-        //encryptedSecret = Cryptographer.encrypt(secret, e, N);
-        
-        
-        
+        encryptedSecret = Cryptographer.encrypt(secret, e, N);
+
         //Print to screen
         System.err.println("New secret: " + secret);
-        System.err.println("Encrypted secrets: ");
-        for (int i = 0; i < encryptedSecrets.size(); i++)
-        {
-            System.out.println(encryptedSecrets.get(i).toString());
-        }
+        System.err.println("Encrypted secret: " + encryptedSecret);
         System.err.flush();
         
         //Send cypher
-        for (int i = 0; i < encryptedSecrets.size(); i++) {
-            out.println(encryptedSecrets.get(i).toString());
-            out.flush();
-        }
-           
+        out.println(encryptedSecret);
+        out.flush();
     }
 
 }
